47aea1caa7ed14cebd17f168b3dd911a054d97b3
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2025-09-02 21:34:37 -0700

    Code added

--- 0242-valid-anagram.swift
+++ 0242-valid-anagram.swift
@@ -1,8 +1,31 @@
-//
-//  0242-Valid-Anagram.swift
-//  
-//
-//  Created by Jeremy Manlangit on 9/2/25.
-//
-
 import Foundation
+
+class Solution {
+    
+    /// Determines whether two given strings are anagrams of each other. .
+    ///
+    /// An anagram is formed by rearranging the characters of a word or phrase
+    /// to produce another word or phrase, using all the original characters exactly once.
+    ///
+    /// - Parameters:
+    ///   - s: The first input string.
+    ///   - t: The second input string.
+    /// - Returns: **true** if `s` and `t` are anagrams of each other, otherwise **false**.
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        guard s.count == t.count else { return false }
+        
+        var charFreq = [Character:Int]()
+        
+        for char in s { charFreq[char, default: 0] += 1 }
+        
+        for char in t {
+            if let count = charFreq[char], count > 0 {
+                charFreq[char] = count - 1
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
